496cdf792f4d570e0648819e71e0492519b95062
GrimmFStack <luisalfonsoekpech@gmail.com>
2025-06-03 18:43:02 -0600

    coño

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,33 @@ import { ValidationPipe } from '@nestjs/common';
 import { BooleanToStringInterceptor } from './interceptors/boolean-to-string.interceptor';
 import { FormatResponseInterceptor } from './interceptors/format-response.interceptor';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+
+  app.setBaseViewsDir(join(__dirname, 'mail', 'templates'));
+  app.setViewEngine('hbs');
+
+  
+  hbs.registerPartials(join(__dirname, 'mail', 'templates', 'partials'));
+
+  
   app.useGlobalInterceptors(
     new FormatResponseInterceptor(),
     new BooleanToStringInterceptor()
   );
 
+ 
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, transform: true })
   );
 
-
+ 
   const config = new DocumentBuilder()
     .setTitle('Hoken API')
     .setDescription('API para el sistema Hoken')
